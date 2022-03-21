d1750973bcc0db1d616403dac0fe940eea03daba
yukatonge <yodaondagobah56@gmail.com>
2022-03-21 01:08:56 -0700

    first-commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = Todo::all();
+        return view('index', ['items' => $items]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+   <div class="container">
+     <div class="card">
+       <p class="title">Todo List</p>
+       <ul>
+         @foreach( $errors->all() as $error)
+         <li>{{$error}}</li> 
+         @endforeach
+       </ul>
+       <div class="todo">
+         <form action="/todo/create" method="post">
+           @csrf
+           <input type="text" name="content">
+           <input type="submit">
+         </form>
+       </div>
+     </div>
+   </div>
+  
+</body>
+</html>
